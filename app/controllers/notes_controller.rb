4512c97fe569e1a1250bec0b15149d80d308796a
Brian Butterly <butterlybrian@gmail.com>
2020-10-22 14:22:16 -0500

    mostly done

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -5,4 +5,32 @@ class NotesController < ApplicationController
         render json: notes
     end
 
+    def create
+        note = Note.create(note_params)
+        render json: note
+    end
+
+    def update
+        note = Note.find(params[:id])
+        note.update(note_params)
+        render json: note
+    end
+
+    def destroy
+        note = Note.find(params[:id])
+        note.destroy
+        render json: note
+    end
+
+    def show
+        note = Note.find(params[:id])
+        render json: note
+    end
+
+end
+
+private
+
+def note_params
+    params.require(:note).permit(:title, :body, :user_id)
 end
